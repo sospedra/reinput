d1ab03c932822d6db25730faaff70bf28c327d5d
sospedra <sospedra.r@gmail.com>
2018-02-06 09:48:07 +0100

    fixup! feat: Add constants

--- src/services/constants.js
+++ src/services/constants.js
@@ -3,5 +3,5 @@ export const BLUE = '#3f51b5'
 export const DURATION = 200
 export const FONT = 15
 export const FONT_SMALL = 12
-export const GRAY = 'gray'
+export const GRAY = '#bdbdbd'
 export const RED = '#fc1f4a'
