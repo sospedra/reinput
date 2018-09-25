7b696b4802378b16b97378f9f29f4b2307f44639
christian <c.martinez@mytaxi.com>
2018-09-25 09:51:48 +0200

    fix: Add more height to input rows

--- src/Error/styles.js
+++ src/Error/styles.js
@@ -3,9 +3,9 @@ import { BASE_UNIT } from '../services/constants'
 export const error = (props = {}) => ({
   color: props.errorColor,
   fontSize: props.errorFontSize,
-  maxHeight: BASE_UNIT * 4
+  height: BASE_UNIT * 4
 })
 
 export const placeholder = () => ({
-  maxHeight: BASE_UNIT * 4
+  height: BASE_UNIT * 4
 })
