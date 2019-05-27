4e8b67d1b0663ff3a9a940e2de257f6b1bd2293c
Rubén Sospedra <r.sospedra@mytaxi.com>
2019-05-27 15:51:13 +0200

    Bump to 3.1.0

--- src/Input/props.js
+++ src/Input/props.js
@@ -1,4 +1,4 @@
-import PropTypes, { InferProps } from 'prop-types'
+import PropTypes from 'prop-types'
 import { TextInput } from 'react-native'
 
 import { BASE_UNIT, BLACK, FONT } from '../services/constants'
